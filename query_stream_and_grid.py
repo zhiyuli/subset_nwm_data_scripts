724c1f870b1392d48fa862050086d96a1f0d464e
zhiyuli <zyli2004@gmail.com>
2017-03-06 16:26:38 -0700

    update

--- query_stream_and_grid.py
+++ query_stream_and_grid.py
@@ -4,9 +4,14 @@ import fiona
 import shapely.wkt
 import shapely.geometry
 import pyspatialite.dbapi2 as db
+import exceptions
 
+# db file full path
+db_file = "/home/drew/Desktop/nwm.sqlite"
+# the default espg code in db
+db_epsg = 4269
 
-def query_stream_comid_and_grid_indices(query_type="shapefile", shp_path=None, geom_str=None,
+def query_comids_and_grid_indices(query_type="shapefile", shp_path=None, geom_str=None,
                                         in_epsg=None, huc_id=None, stream_list=[]):
 
     '''
@@ -23,17 +28,17 @@ def query_stream_comid_and_grid_indices(query_type="shapefile", shp_path=None, g
     query_type_lower = query_type.lower()
     query_stream = True
     query_grid = True
+    query_reservoir = True
     if query_type_lower in ["shapefile", "geojson", "wkt"]:
 
         shape_obj, in_epsg_checked = get_shapely_shape_obj(query_type_lower=query_type_lower, in_epsg=in_epsg,
                                                            shp_path=shp_path, geom_str=geom_str)
     elif "huc" in query_type_lower:
 
-        shape_obj, in_epsg_checked, stream_dict = get_huc_bbox_shapely_shape_obj(huc_type=query_type_lower,
-                                                                                 huc_id=huc_id)
-        query_stream = False
+        shape_obj, in_epsg_checked = get_huc_bbox_shapely_shape_obj(huc_type=query_type_lower,
+                                                                    huc_id=huc_id)
     elif "stream" == query_type_lower:
-        pass
+        raise exceptions.NotImplementedError()
 
     # get the exterior of first polygon
     if shape_obj.geom_type.lower() == "multipolygon":
@@ -44,16 +49,14 @@ def query_stream_comid_and_grid_indices(query_type="shapefile", shp_path=None, g
         raise Exception("Input Geometry is not Polygon")
 
     polygon_query_window = shapely.geometry.Polygon(polygon_exterior_linearring)
-    data = query_stream_and_grid_db(query_wkt=polygon_query_window.wkt, input_epsg=in_epsg_checked,
-                                    query_stream=query_stream, query_grid=query_grid)
+    data = perform_spatial_query(query_window_wkt=polygon_query_window.wkt,
+                                 input_epsg=in_epsg_checked,
+                                 query_stream=query_stream,
+                                 query_grid=query_grid,
+                                 query_reservoir=query_reservoir)
 
-    if not query_stream:
-        data['stream'] = stream_dict
+    print data["grid_land"], data["stream"]["count"], data["reservoir"]["count"]
 
-    print data["grid_land"], data["stream"]["count"]
-    # for comid_spatial_query in  data["stream"]["comids"]:
-    #     if comid_spatial_query not in stream_dict["comids"]:
-    #         print comid_spatial_query
     pass
 
 
@@ -62,7 +65,7 @@ def get_shapely_shape_obj(query_type_lower=None, in_epsg=None, shp_path=None, ge
     if query_type_lower is None:
         raise Exception("Parameter 'query_type_lower' is not given")
 
-    if in_epsg is not None and not check_supported_epsg(in_epsg):
+    if in_epsg is not None and not check_supported_epsg(epsg=in_epsg, db_file=db_file):
         raise Exception("A invalid/unsupported epsg code is given")
 
     in_epsg_checked = in_epsg
@@ -77,9 +80,9 @@ def get_shapely_shape_obj(query_type_lower=None, in_epsg=None, shp_path=None, ge
             print "No epsg code is given. Trying to extract epsg code from shapefile...."
             if "init" in shp_obj.crs:
                 epsg = shp_obj.crs["init"].split(":")[1]
-                if check_supported_epsg(epsg):
+                if check_supported_epsg(epsg=epsg, db_file=db_file):
                     in_epsg_checked = int(epsg)
-                    print in_epsg_checked
+                    print "epsg:", in_epsg_checked
                 else:
                     raise Exception("Shapefile has unsupported projection/epsg code ")
             else:
@@ -114,11 +117,11 @@ def get_shapely_shape_obj(query_type_lower=None, in_epsg=None, shp_path=None, ge
     return shape_obj, in_epsg_checked
 
 
-def check_supported_epsg(in_epsg, db_file = "/home/drew/Desktop/nwm.sqlite"):
+def check_supported_epsg(epsg=None, db_file=None):
 
     conn = None
     try:
-        sql_str = "select count(*) from spatial_ref_sys where auth_srid = {0}".format(in_epsg)
+        sql_str = "select count(*) from spatial_ref_sys where auth_srid = {0}".format(epsg)
         conn = db.connect(db_file)
         cursor = conn.execute(sql_str)
         epsg_count = int(cursor.fetchone()[0])
@@ -133,61 +136,74 @@ def check_supported_epsg(in_epsg, db_file = "/home/drew/Desktop/nwm.sqlite"):
             conn.close()
 
 
-def query_stream_and_grid_db(query_wkt=None, input_epsg=None, query_stream=True, query_grid=True):
-
-    db_epsg=4269
-    db_file = "/home/drew/Desktop/nwm.sqlite"
+def perform_spatial_query(query_window_wkt=None,
+                          input_epsg=None,
+                          query_stream=True,
+                          query_grid=True,
+                          query_reservoir=True):
 
     conn = None
     data = {"status": "success"}
 
     try:
         input_geom = "ST_Transform(GeomFromText('{input_wkt}', {input_epsg}), {db_epsg})".format(
-            input_wkt=query_wkt, input_epsg=input_epsg, db_epsg=db_epsg)
+            input_wkt=query_window_wkt, input_epsg=input_epsg, db_epsg=db_epsg)
 
-
-        # Query Grid Index
-
-        sql_grid_land = 'SELECT min(Grid.west_east) AS minX,\
-        max(Grid.west_east) AS maxX, \
-        min(Grid.south_north) AS minY, \
-        max(Grid.south_north) AS maxY \
-        FROM grid_land as Grid \
-        WHERE \
-        ST_Intersects(Grid.Shape, {input_geom}) = 1 \
-        and \
-        Grid.ROWID IN \
-        (\
-        SELECT ROWID FROM SpatialIndex WHERE f_table_name = "grid_land" \
-        AND search_frame = {input_geom}\
-        );'.format(input_geom=input_geom)
-
-
-        # Query Stream comid
-
-        sql_stream = 'SELECT station_id \
-        FROM stream \
-        WHERE \
-        ST_Intersects(stream.Shape, {input_geom}) = 1 \
-        and \
-        stream.ROWID IN \
-        (\
-        SELECT ROWID FROM SpatialIndex WHERE f_table_name = "stream" \
-        AND search_frame = {input_geom}\
-        );'.format(input_geom=input_geom)
+        sql_template = 'SELECT {query_string} \
+                    FROM {query_table_name} \
+                    WHERE \
+                    ST_Intersects({query_table_name}.{geometry_field_name}, {input_geom}) = 1 \
+                    and \
+                    {query_table_name}.ROWID IN \
+                    (\
+                    SELECT ROWID FROM SpatialIndex WHERE f_table_name = "{query_table_name}" \
+                    AND search_frame = {input_geom}\
+                    );'
 
         conn = db.connect(db_file)
+        geometry_field_name = 'Shape'
 
         if query_grid:
+            query_table_name = 'grid_land'
+            query_string = 'min(grid_land.west_east) AS minX, '\
+                           'max(grid_land.west_east) AS maxX, '\
+                           'min(grid_land.south_north) AS minY, '\
+                           'max(grid_land.south_north) AS maxY'
+
+            sql_grid_land = sql_template.format(query_table_name=query_table_name,
+                                                query_string=query_string,
+                                                geometry_field_name=geometry_field_name,
+                                                input_geom=input_geom)
+
             cursor = conn.execute(sql_grid_land)
             grid_indices = list(cursor.fetchone())
             data['grid_land'] = {"minX": grid_indices[0], "maxX": grid_indices[1],
-                                 "minY": grid_indices[2], "maxY": grid_indices[3], }
-
+                                 "minY": grid_indices[2], "maxY": grid_indices[3]}
         if query_stream:
+            query_table_name = 'stream'
+            query_string = 'station_id'
+
+            sql_stream = sql_template.format(query_table_name=query_table_name,
+                                             query_string=query_string,
+                                             geometry_field_name=geometry_field_name,
+                                             input_geom=input_geom)
             cursor = conn.execute(sql_stream)
-            stream_comids = [item[0].encode('ascii', 'ignore') for item in cursor.fetchall()]
+
+            stream_comids = [item[0] for item in cursor.fetchall()]
             data['stream'] = {"count": len(stream_comids), "comids": stream_comids}
+
+        if query_reservoir:
+            query_table_name = 'reservior'
+            query_string = 'COMID'
+
+            sql_reservoir = sql_template.format(query_table_name=query_table_name,
+                                                query_string=query_string,
+                                                geometry_field_name=geometry_field_name,
+                                                input_geom=input_geom)
+
+            cursor = conn.execute(sql_reservoir)
+            reservior_comids = [item[0] for item in cursor.fetchall()]
+            data['reservoir'] = {"count": len(reservior_comids), "comids": reservior_comids}
 
             conn.close()
 
@@ -212,39 +228,36 @@ def get_huc_bbox_shapely_shape_obj(huc_type=None, huc_id=None):
     elif huc_type == "huc_12" and len(huc_id) != 12:
         raise Exception("Invalid huc_12 comid")
 
-    stream_dict, huc_wkt = query_huc(huc_type=huc_type, huc_id=huc_id)
+    if huc_type not in ["huc_12", "huc_8"]:
+        raise exceptions.NotImplementedError()
+
+    huc_wkt = query_huc(huc_type=huc_type, huc_id=huc_id)
 
     shape_obj = shapely.wkt.loads(huc_wkt)
 
-    return shape_obj, 4269, stream_dict
-
+    return shape_obj, db_epsg
 
 
 def query_huc(huc_type, huc_id):
 
-    db_file = "/home/drew/Desktop/nwm.sqlite"
-    sql_str_comids = 'select station_id from stream where {huc_type} = "{huc_id}"'.format(huc_type=huc_type.upper(),
-                                                                                          huc_id=huc_id)
-    sql_str_huc_wkt = 'select AsWKT(Shape) from huc_12 where {huc_type} = "{huc_id}"'.format(huc_type=huc_type.upper(),
+    # sql_str_comids = 'select station_id from stream where {huc_type} = "{huc_id}"'.format(huc_type=huc_type.upper(),
+    #                                                                                       huc_id=huc_id)
+    sql_str_huc_wkt = 'select AsWKT(Shape) from {huc_type} where {huc_type} = "{huc_id}"'.format(huc_type=huc_type.upper(),
                                                                                              huc_id=huc_id)
     conn = None
-    stream_dict= {}
+
     try:
         conn = db.connect(db_file)
-        cursor = conn.execute(sql_str_comids)
-        stream_comids = [item[0].encode('ascii', 'ignore') for item in cursor.fetchall()]
-        stream_dict["count"] = len(stream_comids)
-        stream_dict["comids"] = stream_comids
-
         cursor = conn.execute(sql_str_huc_wkt)
         huc_wkt = cursor.fetchone()[0].encode('ascii', 'ignore')
 
-        return stream_dict, huc_wkt
+        return huc_wkt
     except Exception as ex:
         raise ex
     finally:
         if conn is not None:
             conn.close()
+
 
 if __name__ == "__main__":
 
@@ -256,15 +269,16 @@ if __name__ == "__main__":
     #               '"coordinates":[[[-109.436035, 43.011979],[-93.361976,42.599528],' \
     #               '[-92.768481,34.295595],[-111.418367,34.515505],[-109.436035, 43.011979]]]}'
     # input_epsg=4326
-    # query_stream_comid_and_grid_indices(query_type="geojson",
+    # query_comids_and_grid_indices(query_type="geojson",
     #                                     geom_str=geojson_str,
     #                                     in_epsg=input_epsg)
 
 
     # input_wkt = "POLYGON((500000 4316776.583097936,500000 4427757.218624833,585360.4618433624 4428236.064519553,586592.6780279021 4317252.164517585,500000 4316776.583097936))"
     # input_epsg = "26912"
-    # query_stream_comid_and_grid_indices(query_type="wkt",
+    # query_comids_and_grid_indices(query_type="wkt",
     #                                     geom_str=input_wkt,
     #                                     in_epsg=input_epsg)
 
-    query_stream_comid_and_grid_indices(query_type="huc_12", huc_id="030902040700")
+    # query_stream_comid_and_grid_indices(query_type="huc_12", huc_id="030902040700")
+    query_comids_and_grid_indices(query_type="huc_8", huc_id="18090203")
