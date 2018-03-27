ac08fab1bd695dc0c3af1b9da15f7427e66412ff
zhiyuli <zyli2004@gmail.com>
2018-03-27 17:32:43 -0600

    1.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='subset_nwm_netcdf',
-      version='1.2.1',
+      version='1.2.2',
       description='Subset National Water Model (NWM) NetCDF',
       url='https://github.com/zhiyuli/subset_nwm_netcdf',
       author='Zhiyu/Drew Li',
